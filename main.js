9d2fe9096fa543cee9f63044438f45ee580addd6
annybarchik <166825324+annybarchik@users.noreply.github.com>
2024-05-09 08:18:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const botões= document.querySelectorAll ("botoes");
+for (let i=0;i <botões.length;i++){
+    botoes[i].onclick=function(){
+        for(let j=0; j <botoes.lenght;j++)
+    }
+{botoes[j].classlist.remove("ativo");
+}
+botoes[i].classlist.add("ativo");
+}
